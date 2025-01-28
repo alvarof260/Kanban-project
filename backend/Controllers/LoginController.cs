04d7035dffb87aef5bac662f1c2ed7219a130784
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2025-01-28 14:50:00 +0000

    refactor: login con cookies

---
+++ backend/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Kanban.Repositories;
+using Kanban.ViewModels;
+using Kanban.Models;
+
+namespace Kanban.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LoginController : ControllerBase
+{
+  private readonly ILogger<LoginController> _logger;
+  private IUsuarioRepository _usuarioRepository;
+
+  public LoginController(ILogger<LoginController> logger, IUsuarioRepository usuarioRepository)
+  {
+    this._logger = logger;
+    this._usuarioRepository = usuarioRepository;
+  }
+
+
+  [HttpPost]
+  public IActionResult Login([FromBody] LoginViewModel model)
+  {
+    if (!ModelState.IsValid)
+    {
+      return BadRequest(ModelState);
+    }
+
+    if (string.IsNullOrEmpty(model.NombreDeUsuario) && string.IsNullOrEmpty(model.Password))
+    {
+      return BadRequest(new { status = "error", mensaje = "Por favor debe ingresar su usuario y contraseña" });
+    }
+
+    Usuario usuario = _usuarioRepository.ObtenerUsuarioNombre(model.NombreDeUsuario);
+
+    if (usuario == null || usuario.Password != model.Password)
+    {
+      return Unauthorized(new { mensaje = "Usuario o contraseña incorrectos" });
+    }
+    HttpContext.Session.SetString("nombre", usuario.NombreDeUsuario);
+    HttpContext.Session.SetString("rol", Convert.ToString(usuario.RolUsuario));
+    _logger.LogInformation("El usuario " + usuario.NombreDeUsuario + " ingreso correctamente");
+    return Ok(new { mensaje = "Autenticacion exitosa", usuario });
+  }
+
+
+
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -11,12 +11,23 @@ string connectionString = builder.Configuration.GetConnectionString("SqliteConne
 
 builder.Services.AddSingleton<string>(connectionString);
 
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+    {
+      options.IdleTimeout = TimeSpan.FromMinutes(30);
+      options.Cookie.HttpOnly = true;
+      options.Cookie.IsEssential = true;
+    });
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseSession();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
