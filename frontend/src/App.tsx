a8ecc06c08afc5f051788fcb682a3839f7b18d49
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2025-02-03 13:39:22 +0000

    refactor: cambiar ProfileSummary con AuthContext

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
 import { Header, SideBar } from './components'
+import { AuthProvider } from './context/auth.context'
 
 // Estructura de clases de tailwind:
 // Caja - flex/grid - Fuente - Colores - Transiciones
 
+
+
 function App() {
+
 
   return (
     <>
       <Header />
-      <SideBar />
+      <AuthProvider>
+        <SideBar />
+      </AuthProvider>
     </>
   )
 }
--- frontend/src/components/SideBar/components/ProfileInfo/ProfileInfo.tsx
+++ frontend/src/components/SideBar/components/ProfileInfo/ProfileInfo.tsx
@@ -1,9 +1,12 @@
+import { useAuthContext } from "../../../../context/auth.context"
 
 export const ProfileInfo = () => {
+  const { user } = useAuthContext()
+
   return (
     <section>
-      <h3 className='text-text-100 text-md font-poppins font-medium'>Alvaro Figueroa</h3>
-      <p className='text-text-200 text-xs font-poppins font-light'>Administrador</p>
+      <h3 className='text-text-100 text-md font-poppins font-medium'>{user?.nombreDeUsuario}</h3>
+      <p className='text-text-200 text-xs font-poppins font-light'>{user?.rolUsuario == 1 ? "Administrador" : "Operandor"}</p>
     </section>
   )
 }
