1d1c229c8fb63b2e324db53d64e542bb3efa2e56
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2025-02-06 18:31:04 +0000

    refactor: cambiar valores del AuthContext

--- frontend/src/context/auth.context.tsx
+++ frontend/src/context/auth.context.tsx
@@ -1,61 +1,36 @@
-import { createContext, ReactNode, useContext, useEffect, useState } from "react";
-import { User } from "../models"
+import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useState } from "react";
+import { User } from "../models";
 
 interface AuthContextType {
   user: User | null;
+  setUser: Dispatch<SetStateAction<User | null>>
 }
 
 interface Props {
   children: ReactNode
 }
 
-const AuthContext = createContext<AuthContextType | null>(null)
+const AuthContext = createContext<AuthContextType>({
+  user: null,
+  setUser: () => { }
+});
 
 export const useAuthContext = () => {
-  const context = useContext(AuthContext)
+  const context = useContext(AuthContext);
 
   if (!context) {
-    throw new Error("useAuth debe estar dentro de de un AuthProvider")
+    throw new Error("useAuth debe estar dentro de de un AuthProvider");
   }
 
-  return context
-}
+  return context;
+};
 
 export const AuthProvider = ({ children }: Props) => {
-  const [user, setUser] = useState<User | null>(null)
-
-  const fetchUser = async (url: string): Promise<User | undefined> => {
-    try {
-      const response = await fetch(url, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          nombreDeUsuario: "Alvarof260",
-          Password: "pepitoDeOro"
-        }),
-        credentials: "include"
-      });
-
-      const data = await response.json();
-
-      if (!data.success) {
-        console.error("Error de autenticación:", data.message);
-        return;
-      }
-
-      setUser(data.data);
-    } catch (error) {
-      console.error("Error:", error)
-    }
-  }
-
-  useEffect(() => {
-    fetchUser("http://localhost:5093/api/login")
-  }, [])
+  const [user, setUser] = useState<User | null>(null);
 
   return (
-    <AuthContext.Provider value={{ user }}>
+    <AuthContext.Provider value={{ user, setUser }}>
       {children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
