48cbcd277fca39d854c65ad6c1aebfa6d98598cd
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2025-02-28 04:00:53 +0000

    refactor: custom modal flexible

--- frontend/src/components/CustomModal/CustomModal.tsx
+++ frontend/src/components/CustomModal/CustomModal.tsx
@@ -10,7 +10,7 @@ interface CustomModalProps {
 export const CustomModal = ({ children, onModal }: CustomModalProps) => {
   return (
     <section className="fixed w-screen h-screen inset-0 flex justify-center items-center bg-black/75">
-      <article className="relative bg-background-secondary border border-accent-dark/30 rounded-md p-6 w-98 h-88 flex flex-col">
+      <article className="relative bg-background-secondary border border-accent-dark/30 rounded-md p-6 w-98 flex flex-col">
         <button
           className="absolute top-3 right-3 h-8 w-8 text-accent-dark/70 text-sm cursor-pointer flex justify-center items-center hover:text-text-light transition duration-300 ease-in "
           onClick={() => onModal("none")}
